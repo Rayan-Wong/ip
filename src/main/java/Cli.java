196dda0e4e39561161f599826a40f0ea570490b2
Rayan-Wong <134691628+Rayan-Wong@users.noreply.github.com>
2025-08-27 12:35:17 +0800

    Align switch-case block in CLI to follow code style

--- src/main/java/Cli.java
+++ src/main/java/Cli.java
@@ -11,34 +11,34 @@ class Cli {
             // strip excess spaces
             String[] cmd = sc.nextLine().replaceAll("\\s+", " ").split(" ");
             switch (cmd[0]) {
-                case "bye":
-                    return;
+            case "bye":
+                return;
 
-                case "add":
-                    if (cmd.length <= 1) {
-                        System.out.println("No task name specified!");
-                    } else {
-                        String task_name = Arrays.stream(cmd, 1, cmd.length).collect(Collectors.joining(" "));
-                        Task task = new Task(task_name);
-                        service.addTask(repo, task);
-                        System.out.println(task_name);
-                    }
-                    break;
+            case "add":
+                if (cmd.length <= 1) {
+                    System.out.println("No task name specified!");
+                } else {
+                    String task_name = Arrays.stream(cmd, 1, cmd.length).collect(Collectors.joining(" "));
+                    Task task = new Task(task_name);
+                    service.addTask(repo, task);
+                    System.out.println(task_name);
+                }
+                break;
 
-                case "list":
-                    List<String> tasks = service.listTasks(repo);
-                    int count = 1;
-                    for (String taskName: tasks) {
-                        System.out.print(count);
-                        System.out.print(". ");
-                        System.out.println(taskName);
-                        count += 1;
-                    }
-                    break;
+            case "list":
+                List<String> tasks = service.listTasks(repo);
+                int count = 1;
+                for (String taskName: tasks) {
+                    System.out.print(count);
+                    System.out.print(". ");
+                    System.out.println(taskName);
+                    count += 1;
+                }
+                break;
 
-                default:
-                    String text = String.join(" ", cmd);
-                    print(text);
+            default:
+                String text = String.join(" ", cmd);
+                print(text);
             }
         }
     }
